f7c19b8df6b4ce4e69c80cf5e6576a3f96320ef8
Arnav Gupta <arnav@codingblocks.com>
2018-10-14 02:39:23 +0530

    headshots are on
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- app/src/main/java/com/tosc/pkbg/ar/game/GameHit.java
+++ app/src/main/java/com/tosc/pkbg/ar/game/GameHit.java
@@ -1,6 +1,8 @@
 package com.tosc.pkbg.ar.game;
 
 public class GameHit {
+    public static final int HIT_HEAD = 0;
+    public static final int HIT_BODY = 1;
 
     public String hitBy;
     public int hitType;
