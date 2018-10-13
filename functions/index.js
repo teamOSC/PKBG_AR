fb499cc75c5f4df6fb2b4e8fd7b24579992c32da
Naman Dwivedi <namandwivedi14@gmail.com>
2018-10-13 20:22:31 +0530

    changes

--- functions/index.js
+++ functions/index.js
@@ -7,27 +7,30 @@ admin.initializeApp();
 exports.checkAndUpdateHit = functions.database.ref('/games/{gameId}/hits/{hitsId}')
   .onCreate((snapshot, context) => {
     const hit = snapshot.val();
+    console.log(hit)
 
     const players = admin.database().ref('/games').child(context.params.gameId).child('players')
 
-    return players.once('value', function (snapshot) {
+    return players.once('value',  (snapshot) => {
 
       var hitTo;
-      snapshot.forEach(function (childSnapshot) {
+      snapshot.forEach((childSnapshot) => {
         var childKey = childSnapshot.key;
         var childData = childSnapshot.val();
+        console.log(childData)
 
         let health = childData.health || 100;
 
         if (childKey !== hit.hitBy) {
           hitTo = childKey
 
-          if (hit.type == 0) {
+          if (hit.type.toString() === '0') {
             health = health - 100;
           }
-          if (hit.type == 1) {
+          if (hit.type.toString() === '1') {
             health = health - 50;
           }
+          console.log(health)
           snapshot.ref.parent.child('players').child(hitTo).child('health').set(health)
         }
 
