7826f94e0ef2f3d9f55297c5506f66da6c1f642a
Arnav Gupta <arnav@codingblocks.com>
2018-10-13 16:31:32 +0530

    taking bitmaps
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- app/src/main/java/com/tosc/pkbg/ar/CustomArFragment.java
+++ app/src/main/java/com/tosc/pkbg/ar/CustomArFragment.java
@@ -1,10 +1,19 @@
 package com.tosc.pkbg.ar;
 
+import android.graphics.Bitmap;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.view.PixelCopy;
+import android.widget.Toast;
+
 import com.google.ar.core.Config;
 import com.google.ar.core.Session;
+import com.google.ar.sceneform.ArSceneView;
 import com.google.ar.sceneform.ux.ArFragment;
 
-public class CustomArFragment extends ArFragment{
+import java.io.IOException;
+
+public class CustomArFragment extends ArFragment {
 
     @Override
     protected Config getSessionConfiguration(Session session) {
@@ -12,5 +21,41 @@ public class CustomArFragment extends ArFragment{
         Config config = super.getSessionConfiguration(session);
         config.setCloudAnchorMode(Config.CloudAnchorMode.ENABLED);
         return config;
+    }
+
+    public Bitmap getBitmap() {
+        ArSceneView view = getArSceneView();
+
+        // Create a bitmap the size of the scene view.
+        return Bitmap.createBitmap(view.getWidth(), view.getHeight(),
+                Bitmap.Config.ARGB_8888);
+    }
+
+    public void captureBitmap() {
+        ArSceneView view = getArSceneView();
+        Bitmap bitmap = getBitmap();
+        // Create a handler thread to offload the processing of the image.
+        final HandlerThread handlerThread = new HandlerThread("PixelCopier");
+        handlerThread.start();
+        // Make the request to copy.
+        PixelCopy.request(view, bitmap, new PixelCopy.OnPixelCopyFinishedListener() {
+            @Override
+            public void onPixelCopyFinished(int copyResult) {
+                if (copyResult == PixelCopy.SUCCESS) {
+                    try {
+                        Utils.saveImage(getActivity(), bitmap);
+                    } catch (IOException e) {
+                        Toast.makeText(getActivity(), e.toString(),
+                                Toast.LENGTH_LONG).show();
+                        return;
+                    }
+                } else {
+                    Toast toast = Toast.makeText(getActivity(),
+                            "Failed to copyPixels: " + copyResult, Toast.LENGTH_LONG);
+                    toast.show();
+                }
+                handlerThread.quitSafely();
+            }
+        }, new Handler(handlerThread.getLooper()));
     }
 }
--- app/src/main/java/com/tosc/pkbg/ar/MainActivity.java
+++ app/src/main/java/com/tosc/pkbg/ar/MainActivity.java
@@ -1,6 +1,7 @@
 package com.tosc.pkbg.ar;
 
 import android.app.AlertDialog;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
@@ -8,6 +9,7 @@ import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.FrameLayout;
 
 import com.google.ar.core.Anchor;
 import com.google.ar.core.HitResult;
@@ -88,6 +90,15 @@ public class MainActivity extends AppCompatActivity {
                 dialog.setOkListener(MainActivity.this::onResolveOkPressed);
                 dialog.show(getSupportFragmentManager(), "Resolve");
 
+            }
+        });
+
+        FrameLayout mainLayout = findViewById(R.id.layout_main);
+        Button shootButton = findViewById(R.id.shoot_button);
+        shootButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fragment.captureBitmap();
             }
         });
 
